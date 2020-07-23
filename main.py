79f4511e11095b0c1205d8a55c76789badbf39ce
Roshan-R <roshanr2001@gmail.com>
2020-07-23 07:49:02 +0530

    added ProgressBar and a requirements.txt file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import urllib.request
+from clint.textui import progress
+import requests
 
 def extract(sem):
     dictionary = {}
     try:
-        sources = open(sem+".txt","r+")
+        sources = open("sources/"+sem+".txt","r+")
         source = sources.read()
         for text in source.split('\n'):
             data = text.split('-')
@@ -23,8 +24,8 @@ def main():
         dictionary = extract(sem)
         x = 1
         for key in dictionary.keys():
-             print('{:3}'.format(str(x)),' : ',key)
-             x += 1
+            print('{:3}'.format(str(x)),' : ',key)
+            x += 1
         num = int(input("\nEnter the number : "))
     nnum = 1
     for key,value in dictionary.items():
@@ -35,8 +36,18 @@ def main():
             nnum += 1
 
 def download(name,url):
+
     location =  name.replace(' ','_') + '.pdf'
-    urllib.request.urlretrieve(url,location)
+    r = requests.get(url, stream=True)
+    with open(location, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+    # os.system("wget -q --show-progress" + url)
+    # urllib.request.urlretrieve(url,location)
 
 if  __name__ == "__main__":
     main()
